c7eb08cb500cf00a8b952b4e562fe6d510014ee7
sophiescob <sophia.escobar1111@gmail.com>
2024-09-11 14:55:22 -0500

    Update fooBar.py

--- fooBar.py
+++ fooBar.py
@@ -1,1 +1,22 @@
-print("prueba conexión con git hub")
+def fooBar(n):
+    """
+    Esta función imprime "Foo", "Bar", o "FooBar" dependiendo de los escenarios de pruebas,
+    o muestra el número en caso de no cumplir con ninguna de las condiciones.
+    """
+
+	# Validar si el numero (n) es divisible entre 3 y 5
+    if n % 3 == 0 and n % 5 == 0:
+        print("FooBar")
+    elif n % 3 == 0:
+        print("Foo")
+    elif n % 5 == 0:
+        print("Bar")
+    else:
+        print(n)
+
+
+# Ejemplos de prueba
+fooBar(9)   # Salida: Foo
+fooBar(10)  # Salida: Bar
+fooBar(15)  # Salida: FooBar
+fooBar(16)  # Salida: 16
